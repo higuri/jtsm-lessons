546907701ec43efcdbf1d125171c8080b5a560ba
higuri <35358263+higuri@users.noreply.github.com>
2018-03-15 22:16:29 +0900

    Add 03_js_es6_modules

---
+++ 03_js_es6_modules/app.js
@@ -0,0 +1,9 @@
+// import function declaration
+// with ES6 syntax.
+//
+// Notice:
+// * [OK] './util.js'
+// * [NG] 'util.js', './util', 'util'
+import {myFunction} from './util.js';
+
+myFunction('Hello');
---
+++ 03_js_es6_modules/util.js
@@ -0,0 +1,8 @@
+var secretHeader = 'Woooooooo!!! ';
+var secretFooter = ' !!!!!!!!!!!!';
+
+// declare & export function
+// with ES6 syntax.
+export function myFunction(s) {
+    alert(secretHeader + s + secretFooter);
+}
